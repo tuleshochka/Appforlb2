72f64e0fa063429e8c0a9c85e67206d63289ac0e
tuleshochka <sizyno.min@gmail.com>
2022-10-16 22:03:07 +0600

    2

--- Transaction/Form2.Designer.cs
+++ Transaction/Form2.Designer.cs
@@ -48,11 +48,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(24, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(475, 21);
+            this.label1.Size = new System.Drawing.Size(447, 21);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Введите название университета, рейтинг которого хотите узнать";
+            this.label1.Text = "Введите имя клиента, информацию о котором хотите узнать";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
             // 
@@ -61,7 +62,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(177, 32);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Отобразить рейтинг";
+            this.button1.Text = "Отобразить";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- Transaction/Form2.cs
+++ Transaction/Form2.cs
@@ -24,14 +24,15 @@ private void button1_Click(object sender, EventArgs e)
         {
             MySqlConnection conn;
             string myConnectionString;
-            myConnectionString = "server=localhost;uid=root;" + "pwd=1515;database=mydb";
+            myConnectionString = "server=localhost;uid=root;" + "pwd=1515;database=database";
             conn = new MySqlConnection(); 
             conn.ConnectionString = myConnectionString;
             conn.Open();
 
             MySqlDataAdapter MyDA = new MySqlDataAdapter();
-            string query = "use mydb; " +
-                "call myProcedure('"+textBox1.Text+"')";
+            //  string query = "use `database`; " +
+            //   "call myProcedure('"+textBox1.Text+"')";
+            string query = "use `database`; call myProcedure ('"+textBox1.Text+"')";
             MyDA.SelectCommand = new MySqlCommand(query, conn);
             DataTable table = new DataTable();
             MyDA.Fill(table);
@@ -39,6 +40,11 @@ private void button1_Click(object sender, EventArgs e)
             bSource.DataSource = table;
             dataGridView1.DataSource = bSource;
             
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
