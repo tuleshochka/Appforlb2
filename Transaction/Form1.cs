d5a4484eb1663a62ba42cde19d43b7ff6b308105
tuleshochka <sizyno.min@gmail.com>
2022-10-16 20:18:54 +0600

    Добавьте файлы проекта.

---
+++ Transaction/Form1.cs
@@ -0,0 +1,43 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Transaction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Добавить_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn;
+            string myConnectionString;
+            myConnectionString = "server=localhost;uid=root;" + "pwd=1515;database=mydb";
+            conn = new MySqlConnection();
+            conn.ConnectionString = myConnectionString;
+            conn.Open();
+            try
+            {
+                string query = "use mydb; call Anketa(\"" + textBox1.Text + "\",\"" + textBox2.Text + "\",\"" + textBox3.Text + "\", \"" + textBox4.Text + "\",\"" + textBox5.Text + "\", \"" + textBox6.Text + "\")";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Изменения внесены");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка");
+            }
+
+        }
+    }
+}
---
+++ Transaction/Form2.cs
@@ -0,0 +1,44 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Transaction
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+       
+private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn;
+            string myConnectionString;
+            myConnectionString = "server=localhost;uid=root;" + "pwd=1515;database=mydb";
+            conn = new MySqlConnection(); 
+            conn.ConnectionString = myConnectionString;
+            conn.Open();
+
+            MySqlDataAdapter MyDA = new MySqlDataAdapter();
+            string query = "use mydb; " +
+                "call myProcedure('"+textBox1.Text+"')";
+            MyDA.SelectCommand = new MySqlCommand(query, conn);
+            DataTable table = new DataTable();
+            MyDA.Fill(table);
+            BindingSource bSource = new BindingSource();
+            bSource.DataSource = table;
+            dataGridView1.DataSource = bSource;
+            
+        }
+    }
+}
